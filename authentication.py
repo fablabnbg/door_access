e0ea637d7a04f86ea7a5ea79ff2d13a09e36a123
pathim <himmelmannp@gmail.com>
2015-09-07 14:55:56 +0200

    Added docstrings to authentication

--- authentication.py
+++ authentication.py
@@ -1,8 +1,30 @@
+"""The authentication module contains classes for different ways of 
+authenticating a NFC uid
+"""
+
 class Auth_file:
+	"""The Auth_file class authenticates against a local file.
+
+	Auth_file(filename)
+	filename : Path to authentication file
+
+	Fileformat
+	----------
+	One record per line. A record consists of three semi-colon separated fields.
+	The first field is the uid in hex bytes separated by spaces.
+	The second field is the access level. An integer
+	The third field is arbitrary information
+	"""
+
 	def __init__(self,filename):
 		self.filename=filename
 	
 	def auth(self,identity):
+		"""Get the access level for the gived uid. The authentication file is reread with every call.
+
+		auth(identity)
+		identity : bytes object with the uid to authenticate. space separated hex-bytes
+		"""
 		try:
 			with open(self.filename,'rb') as f:
 				for l in f:
