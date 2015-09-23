c9918fe7345a6ec8e5d30f6a7df6f26b87d7c5c5
Fablab <fablab@dhcp-172-16-23-152.fablab.lan>
2015-09-24 00:45:08 +0200

    Save diff of permission file to usb on update

--- G25_root/usr/local/door_access/update_permissions.sh
+++ G25_root/usr/local/door_access/update_permissions.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
 
 mount $DEVNAME /mnt 2>&1 >> /home/acme/test.txt
-diff /mnt/door_access /etc/door_access
+diff /mnt/door_access.csv /etc/door_access > /mnt/last_change_tmp.diff
 if [ $? -eq 1 ]; then
-	cp /mnt/door_access /etc/door_access
+	cp /mnt/door_access.csv /etc/door_access
+	mv /mnt/last_change_tmp.diff /mnt/last_change.diff
+	mkdir -p /mnt/history
+
+	lastnum=$(ls /mnt/history | sort -g | tail -n 1)
+	num=$((lastnum+1))
+	cp /mnt/last_change.diff /mnt/history/$num.diff
+	cp /etc/door_access /mnt/history/$num.csv
+else
+	rm /mnt/last_change_tmp.diff
 fi
 umount /mnt
