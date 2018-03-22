42866dfc22302542029e5a712e67c9b8840ed5a9
pathim <himmelmannp@gmail.com>
2018-03-22 14:53:20 +0100

    Fix retrying to close the lock
    Adapt the expected lastwidth downward if the door lock is not detected after 1.5s

--- G25_root/usr/local/door_access/lock_behaviour.py
+++ G25_root/usr/local/door_access/lock_behaviour.py
@@ -20,6 +20,7 @@ class Lock_behaviour:
 		self.closing=False
 		self.open_retry_count=0
 		self.open_time=0
+		self.expected_lastwidth=1
 		if door.is_closed():
 			self.lock.close()
 		else:
@@ -130,14 +131,17 @@ class Lock_behaviour:
 					state=WAIT_LOCK_TURN_FINISHED
 			if state==WAIT_LOCK_TURN_FINISHED:
 				# wait for lock to finish turning
-				if self.lock_led.lastwidth>1:
+				if self.lock_led.lastwidth>self.expected_lastwidth:
 					if callback:
 						callback()
 					break
 				# check if lock actually startet turning
-				if timed_out(start_time,1.5) and self.lock_led.edgecount==0:
-					# lock has not turned after 1.5 seconds. edgecount should be about 10. retry
+				if timed_out(start_time,1.5):
+					if self.lock_led.edgecount>3:
+						# lock has turned after 1.5 seconds but last led-light was not detected. edgecount should be about 10. retry
+						self.expected_lastwidth-=0.1 # expect smaller last_width
 					state=WAIT_CERTAIN_CLOSED_DOOR
+						
 			time.sleep(0.1)
 		time.sleep(1)
 		self.abort=False
